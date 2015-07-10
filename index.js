130357e7968250979d5f2ced44c88bb5350e8b5f
Jason Mitchell <jason.mitchell.w@gmail.com>
2015-07-09 17:31:36 -0700

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 /* jshint node: true */
 'use strict';
 
+var path = require('path');
+var mergeTrees = require('broccoli-merge-trees');
+
+var packageName = 'ember-intl-messageformat';
+
 module.exports = {
-  name: 'ember-intl-messageformat'
+  name: 'intl-messageformat',
+
+  included: function(app) {
+    this._super.included.apply(this, arguments);
+    this.app = app;
+  },
+
+  treeForAddon: function(tree) {
+    var root = path.join(this.project.root, 'node_modules', 'ember-intl-messageformat');
+
+    if (this.app && this.app.project.pkg.name === packageName) {
+      root = path.join(this.project.root);
+    }
+
+    var trees = mergeTrees([this.treeGenerator(path.join(root, 'node_modules', 'intl-messageformat', 'src')), tree], {
+      overwrite: true
+    });
+
+    return this._super.treeForAddon.call(this, trees);
+  }
 };
---
+++ tests/unit/import-test.js
@@ -0,0 +1,21 @@
+import IntlMessageFormat from 'intl-messageformat';
+import { module, test } from 'qunit';
+
+module('Unit | MessageFormat exports');
+
+test('intl-messageformat exports a single module', function(assert) {
+  assert.ok(IntlMessageFormat, 'message-format is exported');
+});
+
+test('intl-messageformat exports a single module', function(assert) {
+  var NUM_PHOTOS = `
+    You have {numPhotos, plural,
+      =0 {no photos.}
+      =1 {one photo.}
+      other {# photos.}
+    }
+  `;
+
+  var enNumPhotos = new IntlMessageFormat(NUM_PHOTOS, 'en-US');
+  assert.equal(enNumPhotos.format({numPhotos: 1000}), 'You have 1,000 photos.');
+});
